da4de69f4a24825b5a738b079d0f1cc2f52c7c5a
mrsteele <matt@omnionline.us>
2018-11-01 19:25:45 -0400

    feat: You now guess the letters to discover

--- pages/pokedex.js
+++ pages/pokedex.js
@@ -25,9 +25,13 @@ const getRandomPokemon = async () => {
   return id
 }
 
+const PokeImage = ({ pokemon, discovered }) => (
+  <img src={pokemon.sprites.front_default} className={`pokeimage ${discovered ? '' : 'undescovered'}`} />
+)
+
 const PokeCard = ({ pokemon, filteredName, discovered }) => (
   <div className={`pokecard ${discovered ? '' : 'undescovered'}`}>
-    <img src={pokemon.sprites.front_default} />
+    <PokeImage pokemon={pokemon} discovered={discovered} />
     <div>
       <h4>{filteredName || pokemon.name}</h4>
       <footer>
@@ -39,47 +43,97 @@ const PokeCard = ({ pokemon, filteredName, discovered }) => (
   </div>
 )
 
+const filterName = (name, guesses) => {
+  const nameArr = name.split('')
+  return nameArr.map(char => guesses.indexOf(char) === -1 ? '_' : char).join('')
+}
+
+const PokeJumbo = ({ pokemon, guesses }) => {
+  const discovered = guesses === true
+  return (
+    <div className='pokejumbo'>
+      <h3>{discovered ? pokemon.name : filterName(pokemon.name, guesses)}</h3>
+      <PokeImage pokemon={pokemon} discovered={discovered} />
+    </div>
+  )
+}
+
+const allLetters = 'abcdefghijklmnopqrstuvwxyz'.split('')
+const Letters = ({ guesses, onClick }) => (
+  <div className='letters'>
+    {allLetters.map(char => <button type="button" disabled={guesses.indexOf(char) !== -1} onClick={e => e.preventDefault && onClick(char)}>{char}</button>)}
+  </div>
+)
+
 class Pokedex extends React.Component {
   state = {
     encounter: null,
-    discovered: []
+    discovered: {}
   }
 
   encounter = async (e) => {
     e.preventDefault()
+    const { discovered } = this.state
     const encounter = await getRandomPokemon()
-    console.log(db[encounter])
-    this.setState({
-      encounter
-    })
+    // set the encounter
+    const newState = { encounter }
+
+    // set the discovered
+    if (!discovered[encounter]) {
+      newState.discovered = {
+        ...discovered,
+        [encounter]: []
+      }
+    }
+    this.setState(newState)
   }
 
-  discover = async (e) => {
-    e.preventDefault()
+  guess = (letter) => {
     const { encounter, discovered } = this.state
-    if (encounter && discovered.indexOf(encounter) === -1) {
-      this.setState({
-        discovered: [...discovered, encounter]
-      })
+    if (encounter && discovered[encounter] && discovered[encounter].indexOf(letter) === -1) {
+      const newArr = [
+        ...discovered[encounter],
+        letter
+      ]
+
+      // check and see if they got then all
+      const name = db[encounter].name
+      console.log('newArr', newArr)
+      if (filterName(name, newArr) === name) {
+        this.setState({
+          discovered: {
+            ...discovered,
+            [encounter]: true
+          }
+        })
+      } else {
+        this.setState({
+          discovered: {
+            ...discovered,
+            [encounter]: newArr
+          }
+        })
+      }
     }
   }
 
   render () {
+    console.log('this.state', this.state)
     const { encounter, discovered } = this.state
     return (
       <main>
         <header>
           <button onClick={this.encounter}>Encounter</button>
-          <button onClick={this.discover}>Scan</button>
         </header>
         {encounter && (
           <section>
-            <PokeCard pokemon={db[encounter]} discovered={discovered.indexOf(encounter) !== -1} />
+            <PokeJumbo pokemon={db[encounter]} guesses={discovered[encounter]} />
+            {discovered[encounter] !== true && <Letters guesses={discovered[encounter]} onClick={this.guess} />}
           </section>
         )}
         <aside>
           {Object.values(db).map(pokemon => (
-            <PokeCard key={pokemon.id} pokemon={pokemon} discovered={discovered.indexOf(pokemon.id) !== -1} />
+            <PokeCard key={pokemon.id} pokemon={pokemon} discovered={discovered[pokemon.id]} />
           ))}
         </aside>
       </main>
