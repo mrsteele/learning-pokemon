1dfb7c4dc53b71583d446b24c91bdde624d384ac
mrsteele <matt@omnionline.us>
2018-11-03 11:35:55 -0400

    feat: adding pokeballs and walking inventory

--- pages/pokedex.js
+++ pages/pokedex.js
@@ -4,8 +4,7 @@ import '../styles/pokedex.scss'
 /**
  * Todos
  * - [ ] Automatically travel to find pokemon (procedural movements on terrain types)
- * - [ ] You can click on pokemon you have discovered to learn more about them.
- * - [ ] More details on the "PokeCard"
+ * - [ ] Create a details pane to see information about discovered pokemon
  */
 const db = {}
 const allLetters = 'abcdefghijklmnopqrstuvwxyz'.split('')
@@ -16,8 +15,9 @@ const request = async (url) => {
   return data
 }
 
+const totalPokemon = 151 // should be 151
 const getRandomPokemon = async () => {
-  const id = Math.floor(Math.random() * 151) + 1
+  const id = Math.floor(Math.random() * totalPokemon) + 1
   if (db[id]) {
     return id
   }
@@ -30,21 +30,35 @@ const PokeImage = ({ pokemon, discovered }) => (
   <img src={pokemon.sprites.front_default} className={`pokeimage ${discovered ? '' : 'undescovered'}`} />
 )
 
-const PokeCard = ({ pokemon, filteredName, discovered }) => (
-  <div className={`pokecard ${discovered ? '' : 'undescovered'}`}>
-    <PokeImage pokemon={pokemon} discovered={discovered} />
-    <div>
-      <h4>{filteredName || pokemon.name}</h4>
-      <footer>
-        {pokemon.types.map(({type}) => (
-          <span className={type.name}>{type.name}</span>
-        ))}
-      </footer>
-    </div>
-  </div>
-)
+class PokeCard extends React.Component {
+  onClick = (e) => {
+    e.preventDefault()
 
-console.log('allLetters', allLetters)
+    const { pokemon, discovered, onClick } = this.props
+
+    if (discovered) {
+      onClick(pokemon.id)
+    }
+  }
+
+  render () {
+    const { pokemon, filteredName, discovered } = this.props
+    return (
+      <div className={`pokecard ${discovered ? '' : 'undescovered'}`} onClick={this.onClick}>
+        <PokeImage pokemon={pokemon} discovered={discovered} />
+        <div>
+          <h4>{filteredName || pokemon.name}</h4>
+          <footer>
+            {pokemon.types.map(({type}) => (
+              <span key={type.name} className={type.name}>{type.name}</span>
+            ))}
+          </footer>
+        </div>
+      </div>
+    )
+  }
+}
+
 const filterName = (name, guesses) => {
   const nameArr = name.split('')
   return nameArr.map(char => guesses.indexOf(char) !== -1 || allLetters.indexOf(char) === -1 ? char : '_').join('')
@@ -62,22 +76,34 @@ const PokeJumbo = ({ pokemon, guesses }) => {
 
 const Letters = ({ guesses, onClick }) => (
   <div className='letters'>
-    {allLetters.map(char => <button type="button" disabled={guesses.indexOf(char) !== -1} onClick={e => e.preventDefault && onClick(char)}>{char}</button>)}
+    {allLetters.map(char => <button key={char} type="button" disabled={guesses.indexOf(char) !== -1} onClick={e => e.preventDefault && onClick(char)}>{char}</button>)}
   </div>
 )
 
 class Pokedex extends React.Component {
   state = {
     encounter: null,
-    discovered: {}
+    discovered: {},
+    won: false,
+    walked: 0,
+    pokeballs: 0
   }
 
   encounter = async (e) => {
     e.preventDefault()
-    const { discovered } = this.state
-    const encounter = await getRandomPokemon()
+    const { discovered, walked } = this.state
+    if (!walked) {
+      return
+    }
+    let encounter = null
+    do {
+      encounter = await getRandomPokemon()
+    } while (discovered[encounter] === true)
     // set the encounter
-    const newState = { encounter }
+    const newState = {
+      encounter,
+      walked: walked - 1,
+    }
 
     // set the discovered
     if (!discovered[encounter]) {
@@ -89,9 +115,17 @@ class Pokedex extends React.Component {
     this.setState(newState)
   }
 
-  guess = (letter) => {
+  setEncounter = (id) => {
     const { encounter, discovered } = this.state
-    if (encounter && discovered[encounter] && discovered[encounter].indexOf(letter) === -1) {
+    if (!encounter || discovered[encounter] === true) {
+      this.setState({ encounter: id })
+    }
+  }
+
+  guess = (letter) => {
+    const { encounter, discovered, pokeballs } = this.state
+    if (pokeballs && encounter && discovered[encounter] && discovered[encounter].indexOf(letter) === -1) {
+      const newPokeballs = pokeballs - 1
       const newArr = [
         ...discovered[encounter],
         letter
@@ -100,16 +134,21 @@ class Pokedex extends React.Component {
       // check and see if they got then all
       const name = db[encounter].name
       if (filterName(name, newArr) === name) {
+        const newDiscovered = {
+          ...discovered,
+          [encounter]: true
+        }
+        const won = (Object.values(newDiscovered).filter(v => v === true).length === totalPokemon)
         this.setState({
-          discovered: {
-            ...discovered,
-            [encounter]: true
-          }
+          won,
+          discovered: newDiscovered,
+          pokeballs: newPokeballs
         })
       } else {
         const newEncounter = name.indexOf(letter) === -1 ? null : encounter
         this.setState({
           encounter: newEncounter,
+          pokeballs: newPokeballs,
           discovered: {
             ...discovered,
             [encounter]: newArr
@@ -119,22 +158,45 @@ class Pokedex extends React.Component {
     }
   }
 
+  add = (prop) => {
+    const value = this.state[prop]
+    this.setState({
+      [prop]: value + 1
+    })
+  }
+
+  addPokeballs = (e) => {
+    e.preventDefault()
+    this.add('pokeballs')
+  }
+
+  addWalk = (e) => {
+    e.preventDefault()
+    this.add('walked')
+  }
+
   render () {
-    const { encounter, discovered } = this.state
+    const { won, walked, pokeballs, encounter, discovered } = this.state
     return (
       <main>
-        <header>
-          <button onClick={this.encounter}>Encounter</button>
-        </header>
+        {!won && (
+          <header>
+            <button onClick={this.encounter} disabled={!walked}>Encounter</button>
+            <button onClick={this.addWalk}>Walk ({walked})</button>
+            <button onClick={this.addPokeballs}>Pokeballs ({pokeballs})</button>
+          </header>
+        )}
         {encounter && (
           <section>
             <PokeJumbo pokemon={db[encounter]} guesses={discovered[encounter]} />
-            {discovered[encounter] !== true && <Letters guesses={discovered[encounter]} onClick={this.guess} />}
+            {discovered[encounter] !== true && (
+              <div>{pokeballs ? <Letters guesses={discovered[encounter]} onClick={this.guess} /> : 'You need pokeballs to guess letters...'}</div>
+            )}
           </section>
         )}
         <aside>
           {Object.values(db).map(pokemon => (
-            <PokeCard key={pokemon.id} pokemon={pokemon} discovered={discovered[pokemon.id] === true} />
+            <PokeCard key={pokemon.id} onClick={this.setEncounter} key={pokemon.id} pokemon={pokemon} discovered={discovered[pokemon.id] === true} />
           ))}
         </aside>
       </main>
